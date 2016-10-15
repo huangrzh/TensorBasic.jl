9846a9598c3012cf9a7b812f4f9a1b473f6a3e0c
Huang RuiZhen <huangrzh@icloud.com>
2016-10-15 16:36:54 +0800

    test file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,6 @@ using TensorBasic
 using Base.Test
 
 # write your own tests here
-@test x = rand(2,3,4); y = tensor_permute(x, [1,2,3], [2,1,3]);
+@eval x = rand(2,3,4);
+@eval y = tensor_permute(x, [1,2,3], [2,1,3]);
+@test y == permutedims(x, [2,1,3]);
